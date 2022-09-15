246c6c7a34e05bbd7bb1cabc29284f96d687f08a
shaikhshehzad <shehzadali3113@gmail.com>
2022-09-15 20:03:43 +0530

    dashboard structure 02

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardParentComponent } from './dashboard-parent/dashboard-parent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardParentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ dashboard/src/app/dashboard-parent/dashboard-parent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-parent',
+  templateUrl: './dashboard-parent.component.html',
+  styleUrls: ['./dashboard-parent.component.css']
+})
+export class DashboardParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
