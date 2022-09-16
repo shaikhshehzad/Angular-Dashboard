f8806a87cd4009b43ecc5bd54e668e73670723d0
shaikhshehzad <shehzadali3113@gmail.com>
2022-09-16 22:51:34 +0530

    Final-setup

--- dashboard/src/app/dashboard-parent/dashboard-parent.component.ts
+++ dashboard/src/app/dashboard-parent/dashboard-parent.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DeviceDetectorService, DeviceInfo } from 'ngx-device-detector';
 @Component({
   selector: 'app-dashboard-parent',
   templateUrl: './dashboard-parent.component.html',
   styleUrls: ['./dashboard-parent.component.css']
 })
-export class DashboardParentComponent implements OnInit {
 
-  constructor() { }
+
+
+export class DashboardParentComponent implements OnInit {
+  deviceInfo:DeviceInfo | undefined;
+  deviceType = 'device-detection-sample';
+  constructor(private deviceDetectorService: DeviceDetectorService) {
+   }
 
   ngOnInit(): void {
+    alert("Make Sure your device is Desktop")
+    this.deviceInfo = this.deviceDetectorService.getDeviceInfo();
+    this.deviceType = this.deviceInfo.deviceType
   }
 
 }
